362a73a6ec8271fcdabfb022aadd922b395d8bc6
socrateslee <socrateslee@users.noreply.github.com>
2022-03-27 10:32:21 +0800

    allow specify timer for decorator perf

--- perfkits/decr.py
+++ perfkits/decr.py
@@ -2,11 +2,11 @@ import functools
 from .line_perf_dbg import LinePerfDbg
 
 
-def perf(log_func=None):
+def perf(log_func=None, timer=None):
     def entangle(func):
         @functools.wraps(func)
         def wrapper(*subs, **kwargs):
-            dbg = LinePerfDbg()
+            dbg = LinePerfDbg(timer=timer)
             ret = dbg.runcall(func, *subs, **kwargs)
             stats = dbg.gen_stats()
             if log_func:
